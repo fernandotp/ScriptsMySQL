f86724a76083f35e713fe8d5c1228226fef1400e
root <root@LAPTOP-KMIG8OSS.localdomain>
2022-01-10 17:21:12 +0100

    BackupAll funcional

--- backupAllBBDD.sh
+++ backupAllBBDD.sh
@@ -3,7 +3,7 @@
 # Este script realiza una copia de seguridad de cada una de las bases de datos existentes en MySQL
 
 TODAY=`date '+%Y_%m_%d__%H_%M_%S'`;
-DIRBACKUPS="/.backup/mysql"
+DIRBACKUPS=/home/fernando/backupsmysql
 
 #Informacion de logueo
 USER="root"		# Nombre de usuario
@@ -16,14 +16,15 @@ MYSQL="$(which mysql)"
 MYSQLDUMP="$(which mysqldump)"
 GZIP="$(which gzip)"
 
-[ ! -d "${DIRBACKUPS}" ] && mkdir -p "${DEST}"
+[ ! -d "${DIRBACKUPS}" ] && mkdir -p "${DIRBACKUPS}"
 
 # Obtener los nombres de todas las bases de datos
 BBDDS="$($MYSQL -u $USER -h $HOST -p $PASS -Bse 'show databases')"
 for bbdd in $BBDDS
 do
 	echo $bbdd
-	#FILE=${DEST}/mysql-${db}.${NOW}-$(date +"%T").gz
-	# get around error
+	NOMBREBACKUP=${DIRBACKUPS}/mysql-${db}.${NOW}-$(date +"%T").gz
+	./backupBBDD.sh $USER $bbdd
+	#mysqldump --user=$USER --password=$PASS --host=$HOST $bbdd > $DIRBACKUPS/$NOMBREBACKUP
 	#$MYSQLDUMP --single-transaction -u $MUSER -h $MHOST -p$MPASS $db | $GZIP -9 > $FILE
 done
--- backupBBDD.sh
+++ backupBBDD.sh
@@ -2,40 +2,41 @@
 dirBackups=/home/fernando/backupsmysql
 
 if [[ (-n "$1") && (-n "$2") ]];
-then 
+then
 	echo -e "\nRealizando backup de $2..."
 	today=`date '+%Y_%m_%d__%H_%M_%S'`;
-	nombreBackup="backup_$2_`date '+%Y-%m-%d_%H:%M:%S'`.sql"
+	nombreBackup="backup_$2_$today.sql"
 
-		#------------REALIZAR BACKUP--------------		
-	
+		#------------REALIZAR BACKUP--------------
+
 	ultimoBackup=$(find $dirBackups -name "*_$2_*" -type f -mtime -9 | tail -1)
-	tamanoUltimoBck=$(du -sh $ultimoBackup)	
+	tamanoUltimoBck=$(du -sh $ultimoBackup)
 	mysqldump --user=$1 --password="" --host=localhost $2 > $dirBackups/$nombreBackup
-	tamanoNuevoBck=$(du -sh  $dirBackups/$nombreBackup)	
+	tamanoNuevoBck=$(du -sh  $dirBackups/$nombreBackup)
 	bckVacio=${tamanoNuevoBck:0:1}
-	
+
 	if [ $bckVacio -eq 0 ]
 	then
 		echo -e "\nError. No se pudo realizar el backup de $2\n"
-		rm $dirBackups/$nombreBackup	
+		rm $dirBackups/$nombreBackup
 	else
-		FICHERO=$dirBackups/$nombreBackup	
+		FICHERO=$dirBackups/$nombreBackup
 		cd  $dirBackups
 
 		if [ -f $nombreBackup ]
 		then
 			if [ "$(ls $dirBackups)" ]
 			then
+
 				if [ "$ultimoBackup" ]
 				then
 					echo -e "\nTamaño del ultimo backup de $2: $tamanoUltimoBck\n"
-				fi				
+				fi
 			fi
 			echo -e "\nTamaño del nuevo backup de $2: $tamanoNuevoBck"
 			echo ""
 			echo "--------------- Espacio file system backups ---------------"
-			df -h $dirBackups 
+			df -h $dirBackups
 			echo ""
 		else
  			echo -e "\nERROR. No se pudo realizar el bakup de $2\n"
