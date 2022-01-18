cbb72f19f0a33d67bf514b359cb9274d11176d20
Cloud User <icenterbbdd@azlvicntrldb03.cpk3obkqofpexdajdnhummwfog.ax.internal.cloudapp.net>
2022-01-18 12:37:17 +0000

    Modificaciones insustanciales

--- comprobarEspacio.sh
+++ comprobarEspacio.sh
@@ -6,5 +6,6 @@ OCUPACION=87
 numeroLineas=$(df -T | awk '$6>$OCUPACION { print $0}' | wc -l )
 if [[ $numeroLineas -gt 1 ]];
 then
-        df -T | awk '$6>69 { print $0}'
+        df -T | awk '$6>$OCUPACION { print $0}'
+
 fi
