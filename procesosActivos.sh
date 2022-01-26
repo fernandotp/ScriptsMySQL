6c8cc60674ff1ed2e4abc9fadf233252ed55e9d5
root <root@azlvicntrldb04.cpk3obkqofpexdajdnhummwfog.ax.internal.cloudapp.net>
2022-01-26 00:22:26 +0000

    Nombre procesos activos actualizado

--- procesosActivos.sh
+++ procesosActivos.sh
@@ -3,8 +3,8 @@
 procesosActivos=$(ps -ef | grep mysql | grep /mysqld)
 if [ "$procesosActivos" ]
 then
-	echo -e "\n---------- Procesos activos de postgres ----------\n"
+	echo -e "\n---------- Procesos activos de MySQL ----------\n"
 	echo $procesosActivos
 else
-	echo -e "\nActualmente no hay ningún proceso de postgres activo\n"
+	echo -e "\nActualmente no hay ningún proceso de MySQL activo\n"
 fi
