0edfd739ecf3b052ee9d86e6e881dae0e85043ab
root <root@azlvicntrldb04.cpk3obkqofpexdajdnhummwfog.ax.internal.cloudapp.net>
2022-02-07 13:11:07 +0000

    Actualización

--- comprobarEspacio.sh
+++ comprobarEspacio.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Este Script muestra todos los filesystems que tengan un porcentaje de ocupacion mayor al indicado
 
-OCUPACION=87
+  numeroLineas=$(df -T | awk '$6>87 { print $0}' | wc -l )
+        if [[ $numeroLineas -gt 1 ]];
+        then
+                df -T | awk '$6>87 { print $0}'
+        fi
 
-numeroLineas=$(df -T | awk '$6>$OCUPACION { print $0}' | wc -l )
-if [[ $numeroLineas -gt 1 ]];
-then
-        df -T | awk '$6>$OCUPACION { print $0}'
-
-fi
--- procesosActivos.sh
+++ procesosActivos.sh
@@ -4,7 +4,8 @@ procesosActivos=$(ps -ef | grep mysql | grep /mysqld)
 if [ "$procesosActivos" ]
 then
 	echo -e "\n---------- Procesos activos de MySQL ----------\n"
-	echo $procesosActivos
+	ps -ef | grep mysql | grep /mysqld | sed -e "$ d"
+	echo ""
 else
 	echo -e "\nActualmente no hay ningún proceso de MySQL activo\n"
 fi
